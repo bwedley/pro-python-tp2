1595af00f0990f4beabc2291084d9b902f71d9ac
bwedley <bruno.wsousa@al.infnet.edu.br>
2024-11-11 20:35:34 -0300

    solves tp2-6 and tp2-12

--- tp2-12.py
+++ tp2-12.py
@@ -4,6 +4,33 @@ as quantidades correspondentes de cada fruta. O programa deve escolher aleatoria
 fornecido for maior do que o número de frutas, o programa deve contabilizar apenas até o último índice da lista de frutas. Caso o número de quantidades seja
 menor do que o número de frutas, o programa deve solicitar ao usuário que reescreva a lista de quantidades (Ex: input do usuário: ‘maçã, banana, laranja’ | ‘10, 5, 8’).
 '''
+import random
 
+def lista_de_frutas():
+    frutas = input("Digite as frutas disponíveis no momento: ").split(",")
+    lista_frutas = [fruta.strip() for fruta in frutas]
+    return lista_frutas
 
+def quantidade_frutas(qtde_frutas):
+    while True:
+        try:
+            qtde_digitada = list(map(int, input("Digite a quantidade de cada fruta: ").split(",")))
+            if len(qtde_digitada) > qtde_frutas:
+                qtde_digitada = qtde_digitada[:qtde_frutas]
+            elif len(qtde_digitada) < qtde_frutas:
+                print("Erro: as quantidades não correspodem.\nPor favor, reveja as quantidades" )
+                continue
+            return qtde_digitada
+        except:
+            print(f"Erro {qtde_digitada}")
 
+def sortear_presente(frutas, qtde):
+    fruta_sorteada = random.choice(frutas)
+    print(f"Parabéns, você ganhou a fruta {fruta_sorteada}!")
+
+def realizar_sorteio():
+    frutas = lista_de_frutas()
+    quantidade = quantidade_frutas(len(frutas))
+    sortear_presente(frutas, quantidade)
+
+realizar_sorteio()
--- tp2-6.py
+++ tp2-6.py
@@ -2,20 +2,21 @@
 Crie uma lista (maior que 5 elementos) e encontre os dois números cuja soma seja o mais próximo possível de zero.
 '''
 
-numeros = [-5, -10, 9, 13, -1, 5, 7]
+numeros = [-2, -10, 9, 13, -1, 5, 7]
+numeros2 = [-139, -123, 231, 223, -766, 872, -321]
 
-first_num = numeros[0]
-second_num = numeros[1]
+def achar_mais_proximo(lista):
+    first_num = lista[0]
+    second_num = lista[1] 
+    mais_proximo_zero = first_num + second_num
+    for i in range(len(lista)):
+        for j in range(i + 1, len(lista)):
+            soma = lista[i] + lista[j]
+            if (abs(soma) < abs(mais_proximo_zero)):
+                mais_proximo_zero = soma
+                first_num = lista[i]
+                second_num = lista[j]
+    print(f'Os dois números somados mais próximo de zero são: {first_num} e {second_num}, que somam {mais_proximo_zero}')
 
-minor = first_num + second_num
-
-for i in range(len(numeros)):
-    for j in range(i + 1, len(numeros)):
-        soma = numeros[i] + numeros[j]
-        if (soma > 0 and (minor <= 0 or soma < minor)) or (soma < 0 and (minor > 0 or soma > minor)):
-            minor = soma
-            first_num = numeros[i]
-            second_num = numeros[j]
-print(f'Os dois números somados mais próximo de zero são: {first_num} e {second_num}, que somam {minor}')
-
-#OBS: ainda não concluido, condicional não satisfaz o exercício
+achar_mais_proximo(numeros)
+achar_mais_proximo(numeros2)
